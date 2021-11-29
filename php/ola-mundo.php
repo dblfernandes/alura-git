4338fb00ce4db12eb377cff9aa4db9b79ffaf0ce
dbfernandes <dbfernandes@live.com>
2021-11-29 15:01:36 -0300

    Curso de PHP

---
+++ php/ola-mundo.php
@@ -0,0 +1,27 @@
+<?php
+
+//Comentário em uma linha
+
+
+/* comentário  
+de
+multiplas 
+linhas
+*/
+
+echo "Olá mundo";
+
+//Declaração de uma variável.
+$idade = 29;
+
+echo $idade;
+
+/*
++ (soma), - (subtração),
+ * (multiplicação),
+ / (divisão),
+ ** (exponenciação),
+ % (módulo ou resto da divisão)
+ **/
+
+ 
